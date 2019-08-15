ac551cecec29f729ff4ee5ebd82d88a764991c21
Michael Chung <michaelchungng@live.com>
2019-08-15 13:08:52 +1200

    Fab button added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import {Row, Col} from 'react-bootstrap';
+import AddIcon from '@material-ui/icons/Add';
 import './App.css';
 
-class App extends React.Component {
+interface IState {
+  vertical: any,
+  horizontal: any,
+}
+
+class App extends React.Component<{}, IState> {
   public render() {
     return (
       <React.Fragment>
         <header className="App-header">
-          <h1 className="App-title">
-            <span className="menlo-font">TO:DO</span>
-            <span> list</span>
-          </h1>
+          <Row>
+            <Col md={6}>
+              <h1 className="App-title">
+                <span className="menlo-font">TO:DO</span>
+                <span> list</span>
+              </h1>
+            </Col>
+            <Col md={6} style={{textAlign: "right"}}>
+              <Button color="inherit" className="Login-button">Login</Button>  
+            </Col>
+          </Row>
         </header>
+        <Fab style={{position: "fixed", bottom: 10, right: 10}}>
+          <AddIcon />
+        </Fab>
       </React.Fragment>
     );
   }
